4c1560e30e60a25b0118006cf70a79b80c563bdf
guuiij <guilherme.jadson@hotmail.com>
2025-01-15 17:23:35 -0300

    "refactor(books): reorganize artifacts into dedicated module and import into app.module"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksController } from './books/books.controller';
 import { BooksModule } from './books/books.module';
-import { BooksService } from './books/books.service';
 
 @Module({
   imports: [BooksModule],
-  controllers: [AppController, BooksController],
-  providers: [AppService,BooksService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
 
 @Module({
-  providers: [BooksService]
+  controllers: [BooksController],
+  providers:  [BooksService]
+    
+  
 })
 export class BooksModule {}
